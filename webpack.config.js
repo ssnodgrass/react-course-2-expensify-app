b63d18f3ccdba33403b621803814fc55855cb345
Scott Snodgrass <github@ssnodgrass.com>
2018-11-19 18:02:02 -0500

    Remove test env

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development' });
 }
 
@@ -62,5 +60,5 @@ module.exports = (env) => {
       historyApiFallback: true,
       publicPath: '/dist/'
     }
-  }
-}
+  };
+};
